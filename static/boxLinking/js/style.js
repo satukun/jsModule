9779292f158dee3591735874cf9789ea71341809
佐藤 陽介 <ysato@C0RNM011.local>
2017-10-12 20:00:20 +0900

    no message

---
+++ static/boxLinking/js/style.js
@@ -0,0 +1,21 @@
+var boxLinking = boxLinking || {};
+
+boxLinking = {
+    Init: {
+        elemWrapper: $("<div>", { class: "wrapper" }),
+        elemList: $("<div>", { class: "list" }),
+        speed: .5,
+        maxHeight: 10,
+        maxWidth: 10,
+        splistsMaxSide: 5,
+        splistsMaxVertical: 20
+    },
+    Calc: function() {
+        var vertical = 100,
+            side = 100,
+            add = 0;
+    }
+
+    // }
+
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,25 @@
 var CA = CA || {};
 CA = {
-    circleFall:function(opt){
-        for(var i = 1; opt.len >= i; i++ ){
-              new circleFall(opt);
+    circleFall: function(opt) {
+        for (var i = 1; opt.len >= i; i++) {
+            new circleFall(opt);
         }
     },
-    boxMove:function(opt){
-        $BODY   = $('body');
-        var i = 0;
-        var yousuke = function(){
-            if(opt.len >= i){
-                new boxMove(opt);
-                i++;
-                setTimeout(yousuke,opt.speed);
-            }
-        };
-        setTimeout(yousuke,0);
+    boxLinking: function() {
+        boxLinking();
     },
-    targetPost:function(opt){
+    targetPost: function(opt) {
         for (var i = 1; opt.lane >= i; i++) {
-          new targetPost(opt, i);
+            new targetPost(opt, i);
         }
     },
-    postTime:function(){
+    postTime: function() {
         postTime();
     },
-    userAgent:function(root){
+    userAgent: function(root) {
         decision(root);
     },
-    slotMacine:function(){
+    slotMacine: function() {
         start();
     }
 }
